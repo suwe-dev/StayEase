2ca0fb2ac667d3985d9bacb0c32c7a1351a28ba3
Suwethan M <suwethan8787@gmail.com>
2024-09-18 07:39:51 +0530

    Initial commit

---
+++ src/test/java/com/example/stayease/StayEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stayease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
